900eb1002a6832a03c8b264786a3fe50b565b334
Robert Good <robert.good@goodtocode.com>
2022-06-08 14:20:13 -0700

    Initial commit - azure-functions

---
+++ azure-functions/typescript/host/Start-Host.ps1
@@ -0,0 +1,19 @@
+﻿# To execute
+#   1. In powershell, set security polilcy for this script: 
+#      Set-ExecutionPolicy Unrestricted -Scope Process -Force
+#   3. Change directory to the script folder:
+#      CD C:\Temp (wherever your script is)
+#   4. In powershell, run script: 
+#      .\Start-Host
+####################################################################################
+Set-ExecutionPolicy Unrestricted -Scope Process -Force
+$VerbosePreference = 'SilentlyContinue' # 'Continue'
+[String]$ThisScript = $MyInvocation.MyCommand.Path
+[String]$ThisDir = Split-Path $ThisScript
+Set-Location $ThisDir # Ensure our location is correct, so we can use relative paths
+Write-Host "*****************************"
+Write-Host "*** Starting: $ThisScript On: $(Get-Date)"
+Write-Host "*****************************"
+####################################################################################
+cd ../
+func host start --useHttps --cert host/certs\dev.myorg.com.pfx --password MyPass1234 --verbose
