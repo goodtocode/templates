40323e91646ae744a1efae7457e29fe8a9978e60
Robert Good <rgood@aacn.org>
2022-06-13 12:52:56 -0700

    Initial commit - bll api

---
+++ architecture-patterns/n-tier/spa-to-api-to-sql/business-logic-api/host/Certs/New-SelfSignedCert.ps1
@@ -0,0 +1,47 @@
+﻿# To execute
+#   1. Run Powershell as ADMINistrator
+#   2. In powershell, set security polilcy for this script: 
+#      Set-ExecutionPolicy Unrestricted -Scope Process -Force
+#   3. Change directory to the script folder:
+#      CD C:\Temp (wherever your script is)
+#   4. In powershell, run script: 
+#      .\New-SelfSignedCert -Path .\dev.myorg.com.pfx -Password MyPass1234 -Dns dev.myorg.com
+
+param (
+	[Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+ 	[string]$Path = $(throw '-Path is a required parameter.'),
+    [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+    [string]$Password = $(throw '-Password is a required parameter.'),
+    [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+	[string]$Dns = $(throw '-Dns is a required parameter.'),
+	[string]$CertStoreLocation = 'cert:\LocalMachine\My'
+)
+####################################################################################
+Set-ExecutionPolicy Unrestricted -Scope Process -Force
+$VerbosePreference = 'SilentlyContinue' # 'Continue'
+[String]$ThisScript = $MyInvocation.MyCommand.Path
+[String]$ThisDir = Split-Path $ThisScript
+Set-Location $ThisDir # Ensure our location is correct, so we can use relative paths
+Write-Host "*****************************"
+Write-Host "*** Starting: $ThisScript On: $(Get-Date)"
+Write-Host "*****************************"
+####################################################################################
+$foundCert = Get-ChildItem -Path $CertStoreLocation | Where-Object Subject -eq "CN=$Dns" | Select-Object *
+if($foundCert.Thumbprint.Length -gt 0)
+{    
+    ## Found, remove it now
+    $thumbprint=$foundCert.Thumbprint
+    Write-host "Found thumbprint $thumbprint"
+    Get-ChildItem "Cert:\LocalMachine\My\$thumbprint" | Remove-Item
+    Write-host "Removed thumbprint $thumbprint"
+    $thumbprint = ""
+}
+
+## Create
+$Certificate = New-SelfSignedCertificate -DnsName $Dns -CertStoreLocation "cert:\LocalMachine\My"
+$thumbprint=$Certificate.Thumbprint
+Write-host "Created certificate with thumbprint $thumbprint"
+
+## Export
+$securePw = ConvertTo-SecureString -String $Password -Force –AsPlainText
+Export-PfxCertificate -Cert "$CertStoreLocation\$thumbprint" -FilePath $Path -Password $securePw
---
+++ architecture-patterns/n-tier/spa-to-api-to-sql/business-logic-api/host/StartHost.cmd
@@ -0,0 +1,9 @@
+﻿
+set curr_dir=%~dp0
+set curr_drive=%curr_dir:~0,2%
+%curr_drive%
+cd %curr_dir%
+cd ../
+
+func host start
+REM func host start --useHttps --cert host/certs\dev.myorg.com.pfx --password MyPass1234 --verbose
