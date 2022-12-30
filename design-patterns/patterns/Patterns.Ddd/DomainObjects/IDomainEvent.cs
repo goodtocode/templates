0e0c48aa31e960710a55622772b88d9af2228519
Robert Good <rgood@aacn.org>
2022-12-30 11:39:10 -0800

    Remvoed design-patterns

--- design-patterns/patterns/Patterns.Ddd/DomainObjects/IDomainEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace GoodToCode.Templates.Patterns.Ddd
-{
-    public interface IDomainEvent<T>
-    {
-        T Item { get; }
-    }
-}   
--- design-patterns/patterns/Patterns.Ddd/DomainObjects/IDomainObject.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace GoodToCode.Templates.Patterns.Ddd
-{
-    public interface IDomainObject
-    {
-        bool Equals(object obj);
-        int GetHashCode();
-    }
-}
