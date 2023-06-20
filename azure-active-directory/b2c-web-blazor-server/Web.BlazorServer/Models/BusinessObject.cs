1679b8e8810ebd7ac57f70143cb217c370ea0533
Robert Good <robert.good@goodtocode.com>
2023-06-19 20:52:46 -0700

    Basic blazor server to Azure B2C

---
+++ azure-active-directory/b2c-web-blazor-server/Web.BlazorServer/Models/BusinessObject.cs
@@ -0,0 +1,7 @@
+﻿namespace Goodtocode.Samples.Domain;
+
+public class BusinessObject
+{
+    public string BusinessName { get; set; } = String.Empty;
+    public string TaxNumber { get; set; } = String.Empty;
+}
