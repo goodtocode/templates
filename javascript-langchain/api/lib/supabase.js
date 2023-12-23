b78b3b78a05ef0eafb995248c45a7a2c0a064cd3
Robert Good <robert.good@goodtocode.com>
2023-12-23 10:27:05 -0800

    Initial commit

---
+++ javascript-langchain/api/lib/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+    const supabaseUrl = "SUPABASE_URL"
+    const supabaseAnon = "SUPABASE_ANON_KEY"
+
+    export const supabase = createClient(supabaseUrl, supabaseAnon)
