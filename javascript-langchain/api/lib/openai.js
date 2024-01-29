94c99742f341e8d29786c62f123a33a1143face1
Robert Good <robert.good@goodtocode.com>
2024-01-28 21:46:12 -0800

    Refresh AI search

--- javascript-langchain/api/lib/openai.js
+++ javascript-langchain/api/lib/openai.js
@@ -1,14 +1,14 @@
 import { ChatOpenAI } from 'langchain/chat_models/openai'
-    import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
 
-    const openaiKey = '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'
+const openaiKey = 'API-KEY'
 
-    export const llm = new ChatOpenAI({
-      openAIApiKey: openaiKey,
-      modelName: 'gpt-3.5-turbo',
-      temperature: 0.9,
-    })
+export const llm = new ChatOpenAI({
+  openAIApiKey: openaiKey,
+  modelName: 'gpt-3.5-turbo',
+  temperature: 0.9,
+})
 
-    export const embeddings = new OpenAIEmbeddings({
-      openAIApiKey: openaiKey,
-    })
+export const embeddings = new OpenAIEmbeddings({
+  openAIApiKey: openaiKey,
+})
